272bf14bf5d491ae2b7a68459ebdd241bdc74de8
X-Cyborg <puneethk.cs18@sahyadri.edu.in>
2020-05-15 18:09:15 +0530

    Create Hack.c
    This code is just 4 fun so please enjoy the following code...

---
+++ Hack.c
@@ -0,0 +1,11 @@
+#include<iostream> 
+using namespace std; 
+int main() { 
+int i,j=0; 
+   cout<<"START ATTACKING............................!!!!!!"<<endl; 
+     for(i=0;i<=5;i++) {
+          cout<<"HACKING NASA...........................................!!!!!!!!!!!!!!"<<j<<"%"<<endl; 
+            j=j+1; }
+              cout<<"NASA HACKED SUCCESFULLY..................................!!!!!!!"<<endl;
+                  return 0; 
+                  }
